35cff793727e24fe29b1813e882c24f464858cd0
Joseph Manalo <manaloja@mcmaster.ca>
2017-11-01 21:12:41 -0400

    Added basic line of sight detection for guards

--- Follicle Abduction/Assets/Patrol.cs
+++ Follicle Abduction/Assets/Patrol.cs
@@ -11,6 +11,12 @@ public class Patrol : MonoBehaviour {
 	
 	private float StoppingDistance = 0.5f;
 
+	// Cone (pie slice, actually) is determined by angle and length
+	public float detectAngle;
+	public float detectRange;
+
+	public GameObject[] detectTargets;
+
 	void NavigateToNextWaypoint() {
 		agent.isStopped = false;
 		
@@ -25,10 +31,43 @@ public class Patrol : MonoBehaviour {
 		agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
 		NavigateToNextWaypoint();
 	}
-	
+
 	void Update () {
-		if(Vector3.Distance(transform.position, navMesh[currentWaypoint]) < StoppingDistance) {
-			NavigateToNextWaypoint();
+		if (Vector3.Distance (transform.position, navMesh [currentWaypoint]) < StoppingDistance) {
+			NavigateToNextWaypoint ();
+		}
+
+		foreach (GameObject target in detectTargets) {
+			if (Detect(target)){
+				Chase(target);
+			}
+		}
+
+	}
+
+	// Check for player within cone of sight
+	bool Detect(GameObject target){
+
+		// Check if target is within detect range
+		if (Vector3.Distance(target.transform.position, this.transform.position) <= detectRange) {
+			
+			// Check if target is within detect angle
+			if (Vector3.Angle (this.transform.forward, target.transform.position - this.transform.position) < detectAngle) {
+				Debug.Log("Detected!");
+				return true;
+			}
 		} 
+
+		return false; 
+	}
+
+	// After detecting, attempt to capture (by chasing)
+	void Chase(GameObject target){
+		Debug.Log("CHASING BOIS");
+	}
+
+	// After successfully catching up to player, capture
+	void Capture(){
+		
 	}
 }
