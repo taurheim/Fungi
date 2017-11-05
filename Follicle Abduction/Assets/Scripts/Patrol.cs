d12991eb7e630283b8bb990e55ff6bdff2006613
Joseph <JosephAQM@gmail.com>
2017-11-05 13:19:59 -0500

    Cleaned up Patrol script and added patrol unit tests

--- Follicle Abduction/Assets/Scripts/Patrol.cs
+++ Follicle Abduction/Assets/Scripts/Patrol.cs
@@ -5,73 +5,62 @@ using UnityEngine.SceneManagement;
 
 public class Patrol : MonoBehaviour {
 
-	public Vector3[] navMesh;
+    public Vector3[] navMesh;
 
-	private UnityEngine.AI.NavMeshAgent agent;
-	private int currentWaypoint = 0;
-	
-	private float StoppingDistance = 0.5f;
+    private UnityEngine.AI.NavMeshAgent agent;
+    private int currentWaypoint = 0;
 
-	// Cone of detection is determined by angle and length
-	public float detectAngle;
-	public float detectRange;
+    private float StoppingDistance = 0.5f;
+
+    // Cone of detection is determined by angle and length
+    public float detectAngle;
+    public float detectRange;
 
     // Array of target objects to look for and try to capture
-	public GameObject[] detectTargets;
+    public GameObject[] detectTargets;
 
-    private GameObject currChaseTarget;
-    private Vector3 lastKnownTargetLoc;
+    public Vector3 currDestination;
+    public GameObject currChaseTarget;
+    public Vector3 lastKnownTargetLoc;
 
-	void NavigateToNextWaypoint() {
-		agent.isStopped = false;
-		
-		// Choose a new waypoint
-		currentWaypoint = (currentWaypoint + 1) % navMesh.Length;
-		agent.SetDestination(navMesh[currentWaypoint]);
+    void NavigateToNextWaypoint() {
 
-		Debug.Log(agent.destination);
-	}
+        // Choose a new waypoint
+        currentWaypoint = (currentWaypoint + 1) % navMesh.Length;
+        currDestination = navMesh[currentWaypoint];
+    }
 
-	void Start () {
-		agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
-		NavigateToNextWaypoint();
-	}
+    void Start() {
+        if (agent != null) {
+            agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        }
+        NavigateToNextWaypoint();
+    }
 
-	void Update () {
+    void Update() {
         // If currently chasing a target
         if (currChaseTarget != null) {
 
             // If target is within reach, capture and resume patrolling
             if (Vector3.Distance(this.transform.position, currChaseTarget.transform.position) < 2) {
                 Capture(currChaseTarget);
-                currChaseTarget = null;
-                agent.SetDestination(navMesh[currentWaypoint]);
-
-                // Return to normal quickness
-                agent.angularSpeed = 120;
-                agent.speed = 3.5f;
+                ResumePatrol();
             }
 
             // Check if target is still in sight, if so, note location and keep chasing
             else if (Detect(currChaseTarget)) {
                 lastKnownTargetLoc = currChaseTarget.transform.position;
-                agent.SetDestination(currChaseTarget.transform.position);
-                
+                currDestination = currChaseTarget.transform.position;
             }
 
             // If patroller reaches last known location, give up and return to patrolling
             else if (Vector3.Distance(this.transform.position, lastKnownTargetLoc) < 1.5) {
-                currChaseTarget = null;
-                agent.SetDestination(navMesh[currentWaypoint]);
-
-                // Return to normal quickness
-                agent.angularSpeed = 120;
-                agent.speed = 3.5f;
+                ResumePatrol();
             }
 
             // If sight of target has been lost, move towards last known location
             else {
-                agent.SetDestination(lastKnownTargetLoc);
+                currDestination = lastKnownTargetLoc;
             }
         }
 
@@ -84,36 +73,66 @@ public class Patrol : MonoBehaviour {
             // Look for all targets in target array, if any found, begin chase
             foreach (GameObject target in detectTargets) {
                 if (Detect(target)) {
-                    currChaseTarget = target;
-                    agent.SetDestination(target.transform.position);
-
-                    // Increase agility, zoomzoom
-                    agent.angularSpeed = 500;
-                    agent.speed = 5f;
+                    Chase(target);
                 }
             }
         }
-	}
 
-	// Check for target within cone of sight
-	bool Detect(GameObject target){
+        // Might have to reduce how often this is called (only when destination changes)
+        MoveToDestination();
+    }
 
-		// Check if target is within detect range
-		if (Vector3.Distance(target.transform.position, this.transform.position) <= detectRange) {
-			
-			// Check if target is within detect angle
-			if (Vector3.Angle(this.transform.forward, target.transform.position - this.transform.position) < detectAngle) {
-				return true;
-			}
-		} 
+    void MoveToDestination() {
+        if (agent != null) {
+            agent.isStopped = false;
+            agent.SetDestination(currDestination);
+        }
+    }
 
-		return false; 
-	}
+    // Check for target within cone of sight
+    public bool Detect(GameObject target) {
 
-	// After successfully catching up to player, capture
-	void Capture(GameObject target){
+        // Check if target is within detect range
+        if (Vector3.Distance(target.transform.position, this.transform.position) <= detectRange) {
+
+            // Check if target is within detect angle
+            if (Vector3.Angle(this.transform.forward, target.transform.position - this.transform.position) < detectAngle) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // After successfully catching up to player, capture
+    void Capture(GameObject target) {
 
         // Placeholder effect, not sure how to do gameover yet
-        target.transform.parent.gameObject.transform.position = new Vector3(-11.8f,-11.01f,0.7f);
+        if (target.transform.parent != null) {
+            target.transform.parent.gameObject.transform.position = new Vector3(-11.8f, -11.01f, 0.7f);
+        }
+
+        currChaseTarget = null;
+    }
+
+    void Chase(GameObject target) {
+        currChaseTarget = target;
+        currDestination = target.transform.position;
+
+        // Increase agility, zoomzoom
+        agent.angularSpeed = 500;
+        agent.speed = 5f;
+    }
+
+    void ResumePatrol() {
+        currChaseTarget = null;
+
+        currDestination = navMesh[currentWaypoint];
+
+        // Return to normal patrol speed
+        if (agent != null) {
+            agent.angularSpeed = 120;
+            agent.speed = 3.5f;
+        }
     }
 }
---
+++ Follicle Abduction/Assets/Tests/PlayMode/PatrolTest.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class PatrolTest {
+
+    [Test]
+    public void DetectTargetInFront() {
+        // Set up our target and guard (patroller) objects
+        GameObject target = new GameObject();
+        target.transform.position = new Vector3(0f, 0f, 0f);
+
+        // Place guard in front of target, and facing target
+        GameObject guard = new GameObject();
+        guard.transform.position = new Vector3(1f, 0f, 0f);
+        guard.transform.forward = new Vector3(-1f, 0f, 0f);
+
+        // Set up Patrol 
+        guard.AddComponent<Patrol>();
+        guard.GetComponent<Patrol>().detectRange = 2;
+        guard.GetComponent<Patrol>().detectAngle = 90;
+
+        bool detected = guard.GetComponent<Patrol>().Detect(target);
+
+        Object.Destroy(target);
+        Object.Destroy(guard);
+
+        Assert.True(detected);
+    }
+
+    [Test]
+    public void DoNotDetectTargetBehind() {
+        // Set up our target and guard (patroller) objects
+        GameObject target = new GameObject();
+        target.transform.position = new Vector3(0f, 0f, 0f);
+
+        // Place guard in front of target, but facing the other way
+        GameObject guard = new GameObject();
+        guard.transform.position = new Vector3(1f, 0f, 0f);
+        guard.transform.forward = new Vector3(1f, 0f, 0f);
+
+        // Set up Patrol 
+        guard.AddComponent<Patrol>();
+        guard.GetComponent<Patrol>().detectRange = 2;
+        guard.GetComponent<Patrol>().detectAngle = 90;
+
+        bool detected = guard.GetComponent<Patrol>().Detect(target);
+
+        Object.Destroy(target);
+        Object.Destroy(guard);
+
+        Assert.False(detected);
+    }
+
+    [UnityTest]
+    public IEnumerator ResumesPatrolling() {
+        Vector3 patrolDestination = new Vector3(10f, 0f, 0f);
+        Vector3 origin = new Vector3(0f, 0f, 0f);
+
+        // Create guard at origin
+        GameObject guard = new GameObject();
+        guard.transform.position = origin;
+
+
+        // Set up patrol
+        guard.AddComponent<Patrol>();
+        guard.GetComponent<Patrol>().lastKnownTargetLoc = origin;
+        guard.GetComponent<Patrol>().navMesh = new Vector3[] { patrolDestination };
+
+        // Create target
+        GameObject target = new GameObject();
+        target.transform.position = new Vector3(10f, 10f, 10f);
+        guard.GetComponent<Patrol>().detectTargets = new GameObject[] { target };
+
+        // Set up Patrol fields
+        guard.GetComponent<Patrol>().detectRange = 0;
+        guard.GetComponent<Patrol>().detectAngle = 0;
+
+        yield return null;
+
+        Object.Destroy(target);
+        Object.Destroy(guard);
+
+        Assert.True(guard.GetComponent<Patrol>().currDestination == patrolDestination);
+    }
+
+    [UnityTest]
+    public IEnumerator CapturesTarget() {
+        // Set up our target and guard (patroller) objects
+        GameObject target = new GameObject();
+        target.transform.position = new Vector3(0f, 0f, 0f);
+
+        // Place guard in front of target, and facing target
+        GameObject guard = new GameObject();
+        guard.transform.position = new Vector3(0.25f, 0f, 0f);
+        guard.transform.forward = new Vector3(-1f, 0f, 0f);
+
+
+        // Set up Patrol 
+        guard.AddComponent<Patrol>();
+        guard.GetComponent<Patrol>().detectRange = 2;
+        guard.GetComponent<Patrol>().detectAngle = 90;
+        guard.GetComponent<Patrol>().currChaseTarget = target;
+
+        Vector3 patrolDestination = new Vector3(10f, 0f, 0f);
+        guard.GetComponent<Patrol>().navMesh = new Vector3[] { patrolDestination };
+
+        guard.GetComponent<Patrol>().detectTargets = new GameObject[] { target };
+
+        yield return null;
+
+        Object.Destroy(target);
+        Object.Destroy(guard);
+
+        Assert.True(guard.GetComponent<Patrol>().currChaseTarget == null);
+    }
+
+
+
+
+}
