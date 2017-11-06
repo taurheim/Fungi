57258da195d4acde02836ed78b6f3e051528d817
Chris Guitar <cagnac@mcmaster.ca>
2017-11-06 06:24:23 -0500

    Added the Human Tests

---
+++ Follicle Abduction/Assets/Tests/PlayMode/PlayerControllerTest.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Characters;
+
+public class PlayerControllerTest : MonoBehaviour {
+
+
+
+	//NOTE: THESE TESTS ARE CURRENTLY EXPECTED TO FAIL! 
+	//This is due to a currently incomplete movement modle which can not be properly tested
+	//using virtual inputs or mock loop inputs as they demand controller input.
+
+	[UnityTest]
+	public IEnumerator PlayerMovesXAxis() 
+	{
+		//uses virtual input to see whether the player moves on x axis and not y
+		GameObject initalPos = new GameObject ();
+		var playerA = new GameObject ().AddComponent<FirstPersonController> ();
+		initalPos.transform.position = new Vector3 (0, 0);
+		playerA.transform.position = initalPos.transform.position;
+
+		CrossPlatformInputManager.SetAxis ("Horizontal", 1);
+
+		yield return null;
+
+		Assert.True(playerA.transform.position.x > initalPos.transform.position.x ||
+			playerA.transform.position.x < initalPos.transform.position.x &&
+			playerA.transform.position.z == initalPos.transform.position.z);
+
+
+	}
+
+	[UnityTest]
+	public IEnumerator PlayerMovesZAxis() 
+	{
+		//uses virtual input to see whether the player moves on y axis and not x
+		GameObject initalPos = new GameObject ();
+		var playerA = new GameObject ().AddComponent<FirstPersonController> ();
+		initalPos.transform.position = new Vector3 (0, 0);
+		playerA.transform.position = initalPos.transform.position;
+
+		yield return null;
+
+		CrossPlatformInputManager.SetAxis ("Virtical", 1);
+
+		Assert.True(playerA.transform.position.z > initalPos.transform.position.z ||
+			playerA.transform.position.z < initalPos.transform.position.z &&
+			playerA.transform.position.x == initalPos.transform.position.x);
+
+
+	}
+
+	[UnityTest]
+	public IEnumerator PlayerMovesXAxisNoInput() 
+	{
+		//uses virtual input to see whether the player moves on x axis and not y
+		GameObject initalPos = new GameObject ();
+		var playerA = new GameObject ().AddComponent<FirstPersonController> ();
+		initalPos.transform.position = new Vector3 (0, 0);
+		playerA.transform.position = initalPos.transform.position;
+
+		yield return null;
+
+		Assert.True(playerA.transform.position.x > initalPos.transform.position.x ||
+			playerA.transform.position.x < initalPos.transform.position.x &&
+			playerA.transform.position.z == initalPos.transform.position.z);
+
+
+	}
+
+	[UnityTest]
+	public IEnumerator PlayerMovesZAxisNoInput() 
+	{
+		//uses virtual input to see whether the player moves on y axis and not x
+		GameObject initalPos = new GameObject ();
+		var playerA = new GameObject ().AddComponent<FirstPersonController> ();
+		initalPos.transform.position = new Vector3 (0, 0);
+		playerA.transform.position = initalPos.transform.position;
+
+		yield return null;
+
+
+		Assert.True(playerA.transform.position.z > initalPos.transform.position.z ||
+			playerA.transform.position.z < initalPos.transform.position.z &&
+			playerA.transform.position.x == initalPos.transform.position.x);
+
+
+	}
+
+
+}
---
+++ Follicle Abduction/Assets/Tests/PlayMode/WinConditionTest.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class WinConditionTest : MonoBehaviour {
+
+
+	GameObject human;
+
+	[SetUp]
+	public void SetUp() {
+
+		//Load test scene
+		Resources.Load ("Prefabs/floor");
+		human = (GameObject)Resources.Load ("FPSController");
+
+
+
+	}
+
+	[TearDown]
+	public void TearDown() {
+		foreach (GameObject obj in GameObject.FindObjectsOfType<GameObject>()) {
+			GameObject.DestroyObject (obj);
+		}
+	}
+
+	[UnityTest]
+	public IEnumerator MovePlayerAwayFromStaticWinAreaAndCheckWinCondition() {
+
+		//"Move" character to away from static win area and check win condition
+		GameObject winArea = new GameObject ();
+		winArea.transform.position = new Vector3 (20, 9);
+		human.transform.position = new Vector3(winArea.transform.position.x+1, winArea.transform.position.y+1);
+		yield return null;
+		Assert.True (winArea.transform.position != human.transform.position);
+
+	}
+
+	[UnityTest]
+	public IEnumerator MovePlayerToStaticWinAreaAndCheckWinCondition() {
+
+		//"Move" character to static win area and check win condition
+		GameObject winArea = new GameObject ();
+		winArea.transform.position = new Vector3 (20, 9);
+		human.transform.position = new Vector3(20, 9);
+		yield return null;
+		Assert.True (winArea.transform.position == human.transform.position);
+
+	}
+		
+	[UnityTest]
+	public IEnumerator FindNewWinArea() {
+
+		//Find new win area in scene
+		GameObject winArea = GameObject.Find("Win Area");
+
+
+		yield return null;
+		Assert.True (winArea.transform != null);
+
+
+	}
+
+	[UnityTest]
+	public IEnumerator MovePlayerAwayFromDynamicWinAreaAndCheckWinCondition() {
+
+		//"Move" character to away from dynamic win area and check win condition
+		GameObject winArea = GameObject.Find("Win Area");
+		human.transform.position = new Vector3(0, 0);
+		yield return null;
+		Assert.True (winArea.transform.position != human.transform.position);
+
+
+	}
+	[UnityTest]
+	public IEnumerator MovePlayerToDynamicWinAreaAndCheckWinCondition() {
+
+		//"Move" character to dynamic win area and check win condition
+		GameObject winArea = GameObject.Find("Win Area");
+		human.transform.position = winArea.transform.position;
+		yield return null;
+		Assert.True (winArea.transform.position == human.transform.position);
+
+
+	}
+	[UnityTest]
+	public IEnumerator WinMessageIsDisplayed() {
+
+		//"Move" character to dynamic win area and check win condition
+		GameObject winArea = GameObject.Find("Win Area");
+		GameObject gameController = GameObject.Find ("Game Controller");
+		human.transform.position = winArea.transform.position;
+		yield return null;
+		Assert.True (gameController == true);
+
+
+	}
+
+
+}
