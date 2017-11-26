9ec8993ea1a9ee2b04f7db22caad4ce8f6019320
huot14 <colehuot14@gmail.com>
2017-11-26 15:00:42 -0500

    New door prefab can be opened and closed

---
+++ Follicle Abduction/Assets/Scripts/DoorAnimation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimation : MonoBehaviour {
+
+    Animation animations;
+
+    [SerializeField]
+    bool startOpen = false;
+
+    bool isOpen = false;
+
+    void Start ()
+    {
+        animations = GetComponent<Animation>();
+
+        // This is just incase the door need to be open at the start
+        if (startOpen)
+        {
+            instantOpen();
+        }
+
+        //close();
+    }
+	
+	public void open()
+    {
+        if (!isOpen)
+        {
+            animations.Play("open", PlayMode.StopAll);
+            isOpen = true;
+        }
+    }
+
+    public void close()
+    {
+        if (isOpen)
+        {
+            animations.Play("close", PlayMode.StopAll);
+            isOpen = false;
+        }
+    }
+
+    void instantOpen()
+    {
+        animations.Play("instantOpen", PlayMode.StopAll);
+        isOpen = true;     
+    }
+}
