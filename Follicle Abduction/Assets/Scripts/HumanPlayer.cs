68a6b41e0e67acc109307ab971e1f00f89881024
bickybilly <bilbilv@mcmaster.ca>
2018-03-08 02:07:36 -0500

    Pick up now works (makes obj disappear, increments count). Also properly committed the HumanPlayer script this time

---
+++ Follicle Abduction/Assets/Scripts/HumanPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanPlayer : MonoBehaviour {
+
+	private int pickups;
+	private Vector3 originalPosition;
+
+	// Use this for initialization
+	void Start () {
+		pickups = 0;
+		originalPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.CompareTag ("pickup")) {
+			other.gameObject.SetActive (false);
+			pickups++;
+		}
+	}
+
+	public void ResetPosition() {
+		transform.position = originalPosition;
+	}
+}
