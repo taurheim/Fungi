a59789b63f80d4be7943f3a19fe0b261fc7c6e32
huot14 <colehuot14@gmail.com>
2018-03-09 15:10:41 -0500

    More changes to the main menu

---
+++ Follicle Abduction/Assets/Scripts/Menus/HostScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HostScreen : MonoBehaviour {
+
+    [SerializeField] Button back;
+    [SerializeField] Text message;      //TODO: Make this blink
+
+    public MenuManager menuManager;
+
+    void Start ()
+    {
+        back.onClick.AddListener(backButton);
+	}
+	
+	void backButton()
+    {
+        menuManager.hideHostScreen();
+        menuManager.displayStartScreen();
+    }
+
+    void onClientConnected()
+    {
+        menuManager.hideHostScreen();
+        menuManager.displayLevelSelect();
+    }
+}
---
+++ Follicle Abduction/Assets/Scripts/Menus/MenuManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    public StartScreen startScreen;
+    public JoinScreen joinScreen;
+    public LevelSelect levelSelect;
+    public HostScreen hostScreen;
+
+	void Start ()
+    {
+
+    }
+	
+    // START SCREEN
+    public void displayStartScreen()
+    {
+        startScreen.gameObject.SetActive(true);
+    }
+
+    public void hideStartScreen()
+    {
+        startScreen.gameObject.SetActive(false);
+    }
+
+    // JOIN SCREEN
+    public void displayJoinScreen()
+    {
+        joinScreen.gameObject.SetActive(true);
+    }
+
+    public void hideJoinScreen()
+    {
+        joinScreen.gameObject.SetActive(false);
+    }
+
+    // LEVEL SELECT
+    public void displayLevelSelect()
+    {
+        levelSelect.gameObject.SetActive(true);
+    }
+
+    public void hideLevelSelect()
+    {
+        levelSelect.gameObject.SetActive(false);
+    }
+
+    // HOST SCREEN
+    public void displayHostScreen()
+    {
+        hostScreen.gameObject.SetActive(true);
+    }
+
+    public void hideHostScreen()
+    {
+        hostScreen.gameObject.SetActive(false);
+    }
+}
