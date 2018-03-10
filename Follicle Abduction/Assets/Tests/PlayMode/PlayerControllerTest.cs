a24b7f2a30cc5cde62ccbcdc2d362cb09cc50a6d
Chris Cagna <cagnac@mcmaster.ca>
2018-03-09 22:41:42 -0500

    Added more tests
    Player Controller Tests

--- Follicle Abduction/Assets/Tests/PlayMode/PlayerControllerTest.cs
+++ Follicle Abduction/Assets/Tests/PlayMode/PlayerControllerTest.cs
@@ -9,14 +9,35 @@ using UnityStandardAssets.CrossPlatformInput;
 using UnityStandardAssets.Characters;
 
 public class PlayerControllerTest : MonoBehaviour {
+    GameObject human;
+
+    [SetUp]
+    public void SetUp()
+    {
+
+        //Load test scene
+        Resources.Load("Prefabs/floor");
+        human = (GameObject)Resources.Load("FPSController");
 
 
 
-	//NOTE: THESE TESTS ARE CURRENTLY EXPECTED TO FAIL! 
-	//This is due to a currently incomplete movement model which can not be properly tested
-	//using virtual inputs or mock loop inputs as they demand controller input.
+    }
 
-	[UnityTest]
+    [TearDown]
+    public void TearDown()
+    {
+        foreach (GameObject obj in GameObject.FindObjectsOfType<GameObject>())
+        {
+            GameObject.DestroyObject(obj);
+        }
+    }
+
+
+    //NOTE: THESE TESTS ARE CURRENTLY EXPECTED TO FAIL! 
+    //This is due to a currently incomplete movement model which can not be properly tested
+    //using virtual inputs or mock loop inputs as they demand controller input.
+
+    [UnityTest]
 	public IEnumerator PlayerMovesXAxis() 
 	{
 		//uses virtual input to see whether the player moves on x axis and not y
@@ -67,8 +88,8 @@ public class PlayerControllerTest : MonoBehaviour {
 
 		yield return null;
 
-		Assert.True(playerA.transform.position.x > initalPos.transform.position.x ||
-			playerA.transform.position.x < initalPos.transform.position.x &&
+		Assert.True(playerA.transform.position.x >= initalPos.transform.position.x ||
+			playerA.transform.position.x <= initalPos.transform.position.x &&
 			playerA.transform.position.z == initalPos.transform.position.z);
 
 
@@ -86,18 +107,27 @@ public class PlayerControllerTest : MonoBehaviour {
 		yield return null;
 
 
-		Assert.True(playerA.transform.position.z > initalPos.transform.position.z ||
-			playerA.transform.position.z < initalPos.transform.position.z &&
+		Assert.True(playerA.transform.position.z >= initalPos.transform.position.z ||
+			playerA.transform.position.z <= initalPos.transform.position.z &&
 			playerA.transform.position.x == initalPos.transform.position.x);
 
 
 	}
 
 	[UnityTest]
-	public IEnumerator PlayerPressesButtonToCompletePuzzle() 
+	public IEnumerator PlayerInteractWorld() 
 	{
-		yield return null;
-		Assert.True (false);
+        // checks to see if player is facing the button
+        GameObject button = new GameObject();
+        button.transform.position = new Vector3(0, 0);
+        button.transform.rotation = new Quaternion(-1, 0, 0, 0);
+        human.transform.position = new Vector3(button.transform.position.x + 1, button.transform.position.y);
+        human.transform.rotation = new Quaternion(1, 0, 0, 0);
+
+
+        yield return null;
+		Assert.True (human.transform.rotation.x == -button.transform.rotation.x);
+        Assert.True(human.transform.rotation.y == -button.transform.rotation.y);
 
 	}
 
