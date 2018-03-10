a87e3b24a21be335956734e4bedc71676aa461d6
Joseph <JosephAQM@gmail.com>
2018-03-09 20:28:06 -0500

    Added ingame menu functionality - Can resume/return to main menu - Does not yet have pause functionality

---
+++ Follicle Abduction/Assets/Scripts/IngameMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngameMenu : MonoBehaviour {
+
+    public Transform canvas;
+
+    // Use this for initialization
+    void Start() {
+        canvas.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            ViewMenu();
+        }
+    }
+
+    public void ViewMenu() {
+        if (!canvas.gameObject.activeInHierarchy) {
+            canvas.gameObject.SetActive(true);
+        }
+        else {
+            canvas.gameObject.SetActive(false);
+        }
+    }
+
+    public void ReturnToMainMenu() {
+        LevelManager.Load("MainMenu");
+    }
+}
