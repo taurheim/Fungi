02a2d44d4d755047aa1c397c7684519908b9f9bb
huot14 <colehuot14@gmail.com>
2018-04-13 15:38:38 -0400

    Fixed alien audio

---
+++ Follicle Abduction/Assets/Scripts/AlienAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Script to disable the alien audio listener if player is the human
+public class AlienAudio : MonoBehaviour {
+
+    CustomNetworkManager manager;
+
+	void Start ()
+    {
+        manager = GameObject.FindGameObjectWithTag("networkmanager").GetComponent<CustomNetworkManager>();
+	}
+
+    void Update()
+    {
+        if (manager.myRole == "human")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+	
+}
