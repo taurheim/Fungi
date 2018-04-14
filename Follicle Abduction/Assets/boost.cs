8f0dc31e5500c692da53bcca146514e08f7dc2f1
Chris Cagna <cagnac@mcmaster.ca>
2018-04-14 06:05:42 -0400

    Added trap doors to level 1
    Trap doors added to level 1

---
+++ Follicle Abduction/Assets/boost.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boost : MonoBehaviour {
+
+    bool hit1;
+    bool hit2;
+
+    public GameObject button;
+
+
+    // Use this for initialization
+    void Start () {
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (button.GetComponentInParent<ButtonPress>().getButtonStatus())
+            hit2 = true;
+
+        if(hit2)
+            transform.Translate(Vector3.back * 10 * 1 * Time.deltaTime);
+
+
+
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "playerA")
+        {
+            hit1 = true;
+            other.transform.parent = transform;
+            other.GetComponent<Rigidbody>().useGravity.Equals(false);
+        }
+
+    }
+}
