2a4f412aace9145f11f2647eb8335fab054f88f8
bickybilly <bilbilv@mcmaster.ca>
2018-04-14 15:23:02 -0400

    Level 3: Fixed Radio

--- Follicle Abduction/Assets/Scripts/RadioNode.cs
+++ Follicle Abduction/Assets/Scripts/RadioNode.cs
@@ -16,83 +16,118 @@ public class RadioNode : Node {
 	public AudioClip[] songs;
 	public int correctSongIndex;
 	public Patrol[] songLovingGuards; // These guards get distracted (move towards the radio) when the correct song is played
-
+	
+	private AudioSource audioSource;
 	private TextMesh songTextDisplay;
-	private AudioSource source;
 	private GameObject radio;
 	private bool playing;
-	private bool choseCorrectSong;
-	private int currentSong;
+	private int currentSelectedSong;
+	private int currentPlayingSong;
 
 
 	public override void initializeNode () {
-
-		source = GetComponent<AudioSource> ();
-		songTextDisplay = transform.Find("SongName").GetComponent<TextMesh>();
 		playing = false;
-		choseCorrectSong = false;
+		currentSelectedSong = -1;
+		currentPlayingSong = -1;
+		songTextDisplay = nodeData.transform.Find("SongName").GetComponent<TextMesh>();
+		audioSource = GetComponent<AudioSource>();
 	}
 	
 	protected override void Update () {
 		base.Update();
 		// Playing music
-		if (playing && !source.isPlaying) {
-			StopPlayingCorrectSong ();
+		if (playing && !audioSource.isPlaying) {
+			NetworkInteract("stopSong");
 		}
-		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-
-		}
-	}
-
-	public void ChooseSong(bool correct) {
-		//NetworkInteract
-	}
-
-	public override void onStartAction() {
-		if (state == NodeState.COMPLETED) {
-			if (choseCorrectSong) {
-				PlayCorrectSong ();
-			} else {
-				PlayWrongSong ();
+		if (isSelected && (state == NodeState.COMPLETED)) {
+			if (Input.GetKeyDown(KeyCode.UpArrow)) {
+				NetworkInteract("incrementSongIndex");
+			} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+				NetworkInteract("decrementSongIndex");
 			}
 		}
 	}
 
-	public override void onEndAction() {
-		return;
+	public override void onStartAction() {
+		if ((state == NodeState.COMPLETED) && (currentSelectedSong >= 0)) {
+			if (playing) {
+				if (currentPlayingSong == currentSelectedSong) {
+					NetworkInteract("stopSong");
+				} else {
+					NetworkInteract("switchSong");
+				}
+			} else {
+				NetworkInteract("startSong");
+			}
+		}
 	}
 
-	public void PlayWrongSong() {
-		//source.PlayOneShot (wrongSong);
+	protected override void Interact(string str) {
+		switch (str) {
+			case "incrementSongIndex":
+				IncrementSongIndex();
+				break;
+			case "decrementSongIndex":
+				DecrementSongIndex();
+				break;
+			case "startSong":
+				StartSong();
+				break;
+			case "switchSong":
+				SwitchSong();
+				break;
+			case "stopSong":
+				StopSong();
+				break;
+		}
 	}
 
-	public void PlayCorrectSong() {
-		if (!playing) {
-			// Actually play the song!
-			// if (source && correctSong){
-			// 	source.Stop ();
-			// 	source.PlayOneShot (correctSong);
-			// }
-			playing = true;
-			if (songLovingGuards != null) {
-				// Attract the guards to the radio!
-				foreach (Patrol guard in songLovingGuards) {
-					GameObject parent = transform.parent.gameObject;;
-					if (parent) {
-						print ("adding secondary target");
-						guard.AddSecondaryTarget (parent);
-					}
+	public void IncrementSongIndex(){
+		currentSelectedSong = (currentSelectedSong + 1) % songs.Length;
+		UpdateSongText();
+	}
+
+	public void DecrementSongIndex(){
+		currentSelectedSong -= 1;
+		Debug.Log("decrement, now song index is " + currentSelectedSong);
+		if (currentSelectedSong < 0) {
+			currentSelectedSong = songs.Length - 1;
+			Debug.Log("out of bounds so we changed it to " + currentSelectedSong);
+		}
+		UpdateSongText();
+	}
+
+	private void UpdateSongText() {
+		songTextDisplay.text = songNames[currentSelectedSong];
+	}
+
+	public void StartSong() {
+		playing = true;
+		currentPlayingSong = currentSelectedSong;
+		audioSource.PlayOneShot(songs[currentPlayingSong]);
+		if ((currentPlayingSong == correctSongIndex) && (songLovingGuards != null)) {
+			// Attract the guards to the radio!
+			foreach (Patrol guard in songLovingGuards) {
+				GameObject parent = transform.parent.gameObject;;
+				if (parent) {
+					print ("adding secondary target");
+					guard.AddSecondaryTarget (parent);
 				}
 			}
 		}
 	}
 
-	public void StopPlayingCorrectSong () {
-		if (playing) {
-			playing = false;
+	public void SwitchSong() {
+		StopSong();
+		StartSong();
+	}
+
+	public void StopSong () {
+		playing = false;
+		audioSource.Stop();
+		if (currentPlayingSong == correctSongIndex){
 			foreach (Patrol guard in songLovingGuards) {
 				GameObject parent = transform.parent.gameObject;
-				print (parent);
 				if (parent) {
 					guard.RemoveSecondaryTarget (parent);
 				}
