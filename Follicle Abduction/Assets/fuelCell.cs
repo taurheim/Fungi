34abd8ad54091542b4733d7ff07f9cddca1e8cdf
Chris Cagna <cagnac@mcmaster.ca>
2018-04-14 17:45:24 -0400

    Added fuel cells interaction lvl2

---
+++ Follicle Abduction/Assets/fuelCell.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fuelCell : MonoBehaviour {
+
+    public int testing = 2;
+    public GameObject cell1;
+    public GameObject cell2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "playerA")
+        {
+            if(cell1.active == false)
+                cell2.transform.position = new Vector3(8, -11.295f, 40.302f);
+                
+        }
+
+    }
+}
